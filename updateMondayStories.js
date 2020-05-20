904c21bfa0018675a3111a5001608289418f0063
Gaston Schvarchman <janeleker@gmail.com>
2020-05-20 00:01:22 -0300

    [Refactor] pivatlStory parameters

--- updateMondayStories.js
+++ updateMondayStories.js
@@ -17,7 +17,7 @@ const getTwoMonthPivotalStories = () => {
   return pivotal.getStoriesFromPeriod(twoMonthAgo,today)
 }
 const filteredMondayRelatedStories = (pivotalStories,mondayItems) => {
-  return pivotalStories.filter(story => new PivotalStory(story).isMondayStory(mondayItems))
+  return pivotalStories.filter(pivotalStory => new PivotalStory(pivotalStory).isMondayStory(mondayItems))
 }
 const formatStory = (pivotalStory,mondayItems) => {
   try {
@@ -32,7 +32,7 @@ const formatStory = (pivotalStory,mondayItems) => {
   catch {return}
 }
 const formatedStories = (pivotalStories,mondayItems) => {
-  return pivotalStories.map(story => formatStory(story,mondayItems)).filter(Boolean)
+  return pivotalStories.map(pivotalStory => formatStory(pivotalStory,mondayItems)).filter(Boolean)
 } 
 const updateMondayStory = ({item_id,columns:{status,texto0}}) => {
   return monday.changeStatusAndSp(item_id,status,texto0)
