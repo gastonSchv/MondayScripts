64b9d7489475ee86c349311eae82d6b4c9d0e5b4
Gaston Schvarchman <janeleker@gmail.com>
2020-05-22 21:37:49 -0300

    [ERROR BRANCHING][Addition] utis.js , createStoriesFromEpic , [Change] createRecenEpicStories

---
+++ createRecentEpicsStories.js
@@ -0,0 +1,32 @@
+const MondayApi = require('../ScriptsApis/MondayApi.js');
+const PivotalApi = require('../ScriptsApis/PivotalApi.js');
+const PivotalStory = require('../ScriptsApis/PivotalStory.js');
+const pivotalConfig = require('../Config/pivotalConfig.js')
+const mondayConfig = require('../Config/mondayConfig.js')
+const natural = require('../ScriptsApis/natural.js')
+const utils = require('./utils.js')
+const moment = require('moment');
+const _ = require('lodash');
+const Promise = require('bluebird')
+
+const monday = new MondayApi(mondayConfig.productBoardId);
+const pivotal = new PivotalApi(pivotalConfig.parsimotionProjectId) 
+
+const filterFunction = ({pivotalStories,pivotalEpics,mondayItems}) => {
+  return _.filter(pivotalStories, pivotalStory => pivotal.isEpicStory(pivotalStory,pivotalEpics))
+} 
+const createEpicStories = () => { 
+  return Promise.props({
+    pivotalStories: pivotal.getNMonthStories(2),
+    pivotalEpics: pivotal.getEpics(),
+    mondayItems: monday.getAllItems()    
+  })
+  .then(({pivotalStories,pivotalEpics,mondayItems}) => utils.creatableStories({pivotalStories,pivotalEpics,mondayItems,filterFunction}))
+  .then(creatableStories => monday.createItems({
+    pivotalStories:creatableStories,
+    groupName:'PivotalBasket'     
+    }))
+  .catch(err => console.log(err))//agregar un then que toma la response y verifique si hubo errores
+}
+
+createEpicStories()
