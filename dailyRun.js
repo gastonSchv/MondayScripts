a278c113660f52d9698829221292c7a2255d3220
Gaston Schvarchman <janeleker@gmail.com>
2020-08-20 18:40:51 -0300

    [Addition] CompareStoriesAndItems method()

--- dailyRun.js
+++ dailyRun.js
@@ -1,12 +1,13 @@
 const createStoriesInPivotal = require('./createStoriesInPivotal.js')
 const createEpicsItems = require('./createRecentEpicsStories.js') 
 const updateItems = require('./updateRecentMondayItems.js')
+const CompareStoriesAndItems = require('./CompareStoriesAndItems.js')
 
 const execute = () => {
 	return createStoriesInPivotal()
 	.then(() => createEpicsItems())
-	.delay(10*1000)
 	.then(() => updateItems())
+	.then(() => CompareStoriesAndItems(1))
 	.catch(err => console.log(err.message))
 }
 
