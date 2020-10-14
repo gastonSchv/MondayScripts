241603be96052838ec197169758e9a7316d41b37
Gaston Schvarchman <janeleker@gmail.com>
2020-10-14 10:57:17 -0300

    [change] in espera filter logic

--- updateRecentMondayItems.js
+++ updateRecentMondayItems.js
@@ -10,20 +10,29 @@ const utils = require('./utils.js')
 const monday = new MondayApi(mondayConfig.productBoardId);
 const pivotal = new PivotalApi(pivotalConfig.parsimotionProjectId) 
 
-const formatStory = (pivotalStory,mondayItems) => {
+const formatStory = (pivotalStory,mondayItems,itemsEnEspera) => {
   try {
+	const item_id = parseInt(new PivotalStory(pivotalStory).getMondayItemId(mondayItems))
+  	
+  	const isInEspera = (item_id,itemsEnEspera) => {
+  	return _(itemsEnEspera)
+  	.some(mondayItem => _.get(mondayItem,'id') == item_id)
+  	}
+
+   	if(isInEspera(item_id,itemsEnEspera)) {return}
+
     return {
-      item_id: parseInt(new PivotalStory(pivotalStory).getMondayItemId(mondayItems)),
+      item_id,
       columns : {
       status : pivotal.convertToMondayStatus(pivotalStory),
       texto0 : _.get(pivotalStory,'estimate','')
       }
     }
   }
-  catch(e){return}
+  catch(e){console.log(e);return}
 }
 const formatedStories = (pivotalStories,mondayItems) => {
-  return pivotalStories.map(pivotalStory => formatStory(pivotalStory,mondayItems)).filter(Boolean)
+  return pivotalStories.map(pivotalStory => formatStory(pivotalStory,mondayItems,filterEnEspera(mondayItems))).filter(Boolean)
 } 
 const updateMondayStory = ({item_id,columns:{status,texto0}}) => {
   return monday.changeStatusAndSp(item_id,status,texto0)
@@ -33,14 +42,14 @@ const updateMondayStories = formattedStories => {
   return Promise.map(formattedStories,updateMondayStory,{concurrency:10})
 }
 const filterEnEspera = items => {
-	return _.filter(items, ({column_values}) => _.isUndefined(_.find(column_values,{text:'En Espera'})))
+	return _.filter(items, ({column_values}) => _.find(column_values,{text:'En Espera'}))
 }
 const executeUpdate = () => {
   return Promise.props({
-    twoMonthPivotalStories: pivotal.getNMonthStories(2),
-    updateableItems: monday.getUpdateableGroupsItems().then(filterEnEspera)
+    twoMonthPivotalStories: pivotal.getNMonthStories(3),
+    updateableItems: monday.getUpdateableGroupsItems()
   })
-  .then(({twoMonthPivotalStories,updateableItems}) => {  
+  .then(({twoMonthPivotalStories,updateableItems}) => {
     return formatedStories(utils.filteredMondayRelatedStories(twoMonthPivotalStories,updateableItems),updateableItems)
     }
   )
